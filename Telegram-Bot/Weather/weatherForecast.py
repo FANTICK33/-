ddf599f643f0394205797613296677a37a78db42
Филипп <111292884+FANTICK33@users.noreply.github.com>
2023-04-15 18:29:49 +0700

    Update weatherForecast.py

--- Telegram-Bot/Weather/weatherForecast.py
+++ Telegram-Bot/Weather/weatherForecast.py
@@ -6,22 +6,26 @@ bot = telebot.TeleBot("5734323883:AAG2ySfHjAFy1EFz7_WWIlCIQmGlFCPsn5I")
 API = '169b2d96e81576b76cf0dbe17e35661e'
 
 # Привествие
+
+
 @bot.message_handler(commands=['start'])
 def profile(message):
-    bot.send_message(message.chat.id, 'Привет, рад тебя видеть! Напиши название города')
+    bot.send_message(
+        message.chat.id, 'Привет, рад тебя видеть! Напиши название города')
 
 
 @bot.message_handler(content_types=['text'])
 def get_weather(message):
     city = message.text.strip().lower()
-    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
+    res = requests.get(
+        f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
     data = json.loads(res.text)
     temp = data["main"]["temp"]
     bot.reply_to(message, f'Сейчас погода: {temp}')
 
     image = 'sun.png' if temp > 5.0 else 'cloud.jpg'
-    file = open('./' + image, 'rb')
-    vbot.send_photo(message.chat.id, file)
+    file = open('./Telegram-Bot/Weather/' + image, 'rb')
+    bot.send_photo(message.chat.id, file)
 
 
 bot.infinity_polling()
